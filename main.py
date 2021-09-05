7732731325517b9058c62121b5f645116d1efd0e
dimasfn <dimasfajriannugroho@gmail.com>
2021-09-05 15:19:55 +0200

    Apply image alignment

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import cv2
 import segmentation as seg
 import align
 
-img = cv2.imread("cards/card_input/input_back_9.png", cv2.IMREAD_COLOR)
-fg_img = seg.white_background_segmentation(img, debug=True, width_ratio=1080)
+# Read image
+input_img = cv2.imread("cards/card_input/input_back_9.png", cv2.IMREAD_COLOR)
+# Apply segmentation
+fg_img = seg.white_background_segmentation(input_img, debug=False, width_ratio=1080)
+
+# Apply image alignment with the image template
+template_img = cv2.imread("cards/card_full/back_full_1.jpg", cv2.IMREAD_COLOR)
+aligned_image = align.align_image(fg_img, template_img, maxFeatures=50000, keepPercent=2, debug=True)
+
+seg.show_image(debug_state=True, image=aligned_image, show_area=1080)
