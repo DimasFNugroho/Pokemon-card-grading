2cf9d3f4723923f951c6a27cc1f670a392df6af0
dimasfn <dimasfajriannugroho@gmail.com>
2021-09-05 14:55:33 +0200

    Try segmentation using only one image sample

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import cv2
 import segmentation as seg
-
-img = cv2.imread("cards/card_input/input_back_1.png", cv2.IMREAD_COLOR)
-fg_img = seg.white_background_segmentation(img, debug=True, show_ratio=0.8)
+import align
 
 img = cv2.imread("cards/card_input/input_back_9.png", cv2.IMREAD_COLOR)
-fg_img = seg.white_background_segmentation(img, debug=True, show_ratio=0.8)
-
-img = cv2.imread("cards/card_input/input_back_bad_1.jpeg", cv2.IMREAD_COLOR)
-fg_img = seg.white_background_segmentation(img, debug=True, show_ratio=0.8)
-
-img = cv2.imread("cards/card_input/input_back_bad_2.jpeg", cv2.IMREAD_COLOR)
-fg_img = seg.white_background_segmentation(img, debug=True, show_ratio=0.2)
-
-img = cv2.imread("cards/card_input/input_cleffa.png", cv2.IMREAD_COLOR)
-fg_img = seg.white_background_segmentation(img, debug=True, show_ratio=0.8)
-
-img = cv2.imread("cards/card_input/input_lurantis.png", cv2.IMREAD_COLOR)
-fg_img = seg.white_background_segmentation(img, debug=True, show_ratio=0.8)
+fg_img = seg.white_background_segmentation(img, debug=True, width_ratio=1080)
