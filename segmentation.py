b75af0e618fafa8d221865d8d3bf73d7c9605daf
dimasfn <dimasfajriannugroho@gmail.com>
2021-09-07 02:18:12 +0200

    Create conditional statement for each debugging prints

--- segmentation.py
+++ segmentation.py
@@ -6,22 +6,26 @@ import display_image as dis
 def white_background_segmentation(img, debug=False, width_ratio=1080):
 
 	# 1. convert the image to RGB
-	print("1. Read_image")
+	if debug == True:
+		print("1. Read_image")
 	dis.show_image(debug, img, width_ratio)
 
 	# 2.  Apply Median Blur
 	median = cv2.medianBlur(img, 13)
-	print("2. Apply median filter")
+	if debug == True:
+		print("2. Apply median filter")
 	dis.show_image(debug, median, width_ratio)
 
 	# 3. get negative image
 	img_neg = cv2.bitwise_not(median)
-	print("3. Get inverted image")
+	if debug == True:
+		print("3. Get inverted image")
 	dis.show_image(debug, img_neg, width_ratio)
 
 	# 4. Convert the image to gray-scale
 	gray = cv2.cvtColor(img_neg, cv2.COLOR_BGR2GRAY)
-	print("4. Get grayscale")
+	if debug == True:
+		print("4. Get grayscale")
 	dis.show_image(debug, gray, width_ratio)
 
 	# 5. Apply thresholding
@@ -29,26 +33,31 @@ def white_background_segmentation(img, debug=False, width_ratio=1080):
 	h, w = img_thresh.shape[:2]
 	mask = np.zeros((h+2, w+2), np.uint8)
 
-	print("flood")
+	if debug == True:
+		print("flood")
 	cv2.floodFill(img_thresh, mask, (0,0), 127)
 	dis.show_image(debug, img_thresh, width_ratio)
 
 	(thresh, img_thresh_white) = cv2.threshold(img_thresh, 250, 255, cv2.THRESH_BINARY)
-	print("white")
+	if debug == True:
+		print("white")
 	dis.show_image(debug, img_thresh_white, width_ratio)
 
 	img_thresh_neg = cv2.bitwise_not(img_thresh)
 	(thresh, img_thresh_black) = cv2.threshold(img_thresh_neg, 250, 255, cv2.THRESH_BINARY)
-	print("black")
+	if debug == True:
+		print("black")
 	dis.show_image(debug, img_thresh_black, width_ratio)
 
 	img_thresh = img_thresh_white + img_thresh_black
-	print("5. Apply thresholding")
+	if debug == True:
+		print("5. Apply thresholding")
 	dis.show_image(debug, img_thresh, width_ratio)
 
 	# 6. Find the edges
 	edges = cv2.Canny(img_thresh, 100, 200)
-	print("6. Get edges")
+	if debug == True:
+		print("6. Get edges")
 	dis.show_image(debug, edges, width_ratio)
 
 	contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -58,13 +67,15 @@ def white_background_segmentation(img, debug=False, width_ratio=1080):
 	for i in range(len(contours)):
 		hull = cv2.convexHull(contours[i])
 	cv2.drawContours(convex_hull, [hull], -1, (255, 0, 0), -1)
-	print("7. Get convex hull")
+	if debug == True:
+		print("7. Get convex hull")
 	dis.show_image(debug, convex_hull, width_ratio)
 
 	# 8. Apply bitwise operation between convex hull result and the input image
 	convex_hull = cv2.cvtColor(convex_hull, cv2.COLOR_GRAY2RGB)
 	masked = cv2.bitwise_and(img, convex_hull)
-	print("8. Segmentation result")
+	if debug == True:
+		print("8. Segmentation result")
 	dis.show_image(debug, masked, width_ratio)
 
 	return masked
